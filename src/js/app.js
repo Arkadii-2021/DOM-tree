0956dcebf1bb01d06ead9b56d7a5ce1db8e98b86
arkadii <88792842+Arkadii-2021@users.noreply.github.com>
2022-10-11 11:49:45 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,23 +4,33 @@ export default class NewCell {
     this.activecell = 1;
   }
 
+  static makeCell() {
+    const parentEl = document.querySelector('.cell-game');
+    for (let i = 1; i <= 16; i += 1) {
+      const div = document.createElement('div');
+      div.id = `cell${i}`;
+      div.classList = 'cell';
+      parentEl.appendChild(div);
+    }
+  }
+
   static getCell(index) {
     return document.getElementById(`cell${index}`);
   }
 
   static deactivateCell(index) {
-    this.getCell(index).classList = 'cell';
+    NewCell.getCell(index).classList = 'cell';
   }
 
   static activatecell(index) {
-    this.getCell(index).classList.toggle('cell_has-goblin');
+    NewCell.getCell(index).classList.toggle('cell_has-goblin');
   }
 
   nextCell() {
     const next = () => setTimeout(() => {
-      this.deactivateCell(this.activecell);
+      NewCell.deactivateCell(this.activecell);
       this.activecell = Math.floor(1 + Math.random() * 9);
-      this.activatecell(this.activecell);
+      NewCell.activatecell(this.activecell);
       next();
     }, 1200);
     next();
